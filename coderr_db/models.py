155174998e00f3b3f8dcfc461ddfc46bccd5e250
Alexander Hardtke <5yl3kz@googlemail.com>
2025-03-14 08:47:46 +0100

    increased UserProfil

--- coderr_db/models.py
+++ coderr_db/models.py
@@ -9,3 +9,7 @@ class UserProfil(models.Model):
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=10, choices=CATEGORY_CHOICES, default='customer')
+    email = models.EmailField(max_length=50)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    username = first_name + ' ' + last_name
