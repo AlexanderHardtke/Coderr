bd7581086d3bb28b07d9300c063631bebaa9e02d
Alexander Hardtke <5yl3kz@googlemail.com>
2025-03-16 14:12:09 +0100

    login view functional

--- coderr_db/api/urls.py
+++ coderr_db/api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import RegistrationView, UserListBusinessViewSet, UserListCustomerViewSet
+from .views import RegistrationView, UserListBusinessViewSet, UserListCustomerViewSet, LoginView
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -12,7 +12,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     # path('', include(router.urls)),
     path('registration/', RegistrationView.as_view(), name='registration-detail'),
-    path('login/', UserListBusinessViewSet.as_view(), name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('profile/<int:pk>/', UserListBusinessViewSet.as_view(), name='profile-detail'),
     path('profiles/business/', UserListBusinessViewSet.as_view(), name='profiles-list'),
     path('profiles/customer/', UserListCustomerViewSet.as_view(), name='profiles-list'),
