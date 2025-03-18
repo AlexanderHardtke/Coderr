e3734c196e9c25b3dae456d2e451d52ce8d4ed07
Alexander Hardtke <5yl3kz@googlemail.com>
2025-03-18 13:52:40 +0100

    tests for profile done

--- coderr_db/tests/test_profiles.py
+++ coderr_db/tests/test_profiles.py
@@ -34,6 +34,19 @@ class ProfileTests(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_get_unauthorized_user(self):
+        url = reverse('profile-detail', kwargs={'pk': self.user.pk})
+        unauthorized_token = 'unauthorized token'
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + unauthorized_token)
+        self.user = User.objects
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_profile_not_found(self):
+        url = reverse('profile-detail', kwargs={'pk': self.user.pk+1})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
     def test_update_profile(self):
         url = reverse('profile-detail', kwargs={'pk': self.user.pk})
         data = {
@@ -58,11 +71,21 @@ class ProfileTests(APITestCase):
         self.assertIsInstance(response.data['type'], str)
         self.assertIsInstance(response.data['created_at'], str)
 
-    def test_unauthorized_user(self):
+    #sind irgendwie doppelt
+    def test_update_unauthorized_user(self):
         url = reverse('profile-detail', kwargs={'pk': self.user.pk})
+        unauthorized_token = 'unauthorized token'
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + unauthorized_token)
         self.user = User.objects
-        response = self.client.get(url)
-        #
+        data = {"first_name": "error"}
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        
+    def test_update_profile_not_found(self):
+        url = reverse('profile-detail', kwargs={'pk': self.user.pk+1})
+        data = {"first_name": "error"}
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_unauthorized_owner(self):
         url = reverse('profile-detail', kwargs={'pk': self.user.pk})
@@ -75,7 +98,3 @@ class ProfileTests(APITestCase):
         data = {"first_name": "error"}
         response = self.client.patch(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_user_not_found(self):
-        #
-        pass
--- coderr_db/tests/test_registration_login.py
+++ coderr_db/tests/test_registration_login.py
@@ -1,89 +1,89 @@
-# from django.urls import reverse
-# from rest_framework.test import APIClient, APITestCase
-# from rest_framework import status
-# from rest_framework.authtoken.models import Token
-# from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.test import APIClient, APITestCase
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 
 
-# class CreateUserTests(APITestCase):
+class CreateUserTests(APITestCase):
 
-#     def setUp(self):
-#         self.client = APIClient()
-#         self.url = reverse('registration-detail')
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('registration-detail')
 
-#     def test_create_user(self):
-#         data = {
-#             "username": "exampleUsername",
-#             "email": "example@mail.de",
-#             "password": "examplePassword",
-#             "repeated_password": "examplePassword",
-#             "type": "customer"
-#         }
-#         response = self.client.post(self.url, data, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    def test_create_user(self):
+        data = {
+            "username": "exampleUsername",
+            "email": "example@mail.de",
+            "password": "examplePassword",
+            "repeated_password": "examplePassword",
+            "type": "customer"
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-#         partial_response = {
-#             "username": "exampleUsername",
-#             "email": "example@mail.de",
-#         }
-#         for key, value in partial_response.items():
-#             self.assertEqual(response.data[key], value)
+        partial_response = {
+            "username": "exampleUsername",
+            "email": "example@mail.de",
+        }
+        for key, value in partial_response.items():
+            self.assertEqual(response.data[key], value)
 
-#         self.assertIsInstance(response.data['token'], str)
-#         self.assertIsInstance(response.data['user_id'], int)
+        self.assertIsInstance(response.data['token'], str)
+        self.assertIsInstance(response.data['user_id'], int)
 
-#         response_duplicate = self.client.post(self.url, data, format='json')
-#         self.assertWarnsMessage(response_duplicate.data,
-#                                 'A user with that username already exists.')
+        response_duplicate = self.client.post(self.url, data, format='json')
+        self.assertWarnsMessage(response_duplicate.data,
+                                'A user with that username already exists.')
 
-#     def test_fail_create_user(self):
-#         data = {
-#             "username": "exampleUsername",
-#             "email": "example@mail.de",
-#             "password": "rightPassword",
-#             "repeated_password": "wrongPassword",
-#             "type": "customer"
-#         }
-#         response = self.client.post(self.url, data, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_fail_create_user(self):
+        data = {
+            "username": "exampleUsername",
+            "email": "example@mail.de",
+            "password": "rightPassword",
+            "repeated_password": "wrongPassword",
+            "type": "customer"
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-#     def test_get_create_user(self):
-#         response = self.client.get(self.url)
-#         self.assertEqual(response.status_code,
-#                          status.HTTP_405_METHOD_NOT_ALLOWED)
+    def test_get_create_user(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code,
+                         status.HTTP_405_METHOD_NOT_ALLOWED)
 
-#     # Wie schreibe ich einen Test für HTTP_500?
-#     # def test_false_method(self):
-#     #     self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+    # Wie schreibe ich einen Test für HTTP_500?
+    # def test_false_method(self):
+    #     self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-# class UserLoginTests(APITestCase):
+class UserLoginTests(APITestCase):
 
-#     def setUp(self):
-#         self.user = User.objects.create_user(
-#             username='testuser', password='testpassword')
-#         self.client = APIClient()
-#         self.token = Token.objects.create(user=self.user)
-#         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-#         self.url = reverse('login')
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
+        self.client = APIClient()
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.url = reverse('login')
 
-#     def test_login_user(self):
-#         data = {
-#             "username": "testuser",
-#             "password": "testpassword"
-#         }
-#         response = self.client.post(self.url, data, format='json')
+    def test_login_user(self):
+        data = {
+            "username": "testuser",
+            "password": "testpassword"
+        }
+        response = self.client.post(self.url, data, format='json')
 
-#         for key, value in data.items():
-#             self.assertEqual(data[key], value)
-#         self.assertIsInstance(response.data['token'], str)
-#         self.assertIsInstance(response.data['user_id'], int)
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        for key, value in data.items():
+            self.assertEqual(data[key], value)
+        self.assertIsInstance(response.data['token'], str)
+        self.assertIsInstance(response.data['user_id'], int)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-#     def test_fail_login_user(self):
-#         data = {
-#             "username": "wrongUsername",
-#             "password": "wrongPassword"
-#         }
-#         response = self.client.post(self.url, data, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_fail_login_user(self):
+        data = {
+            "username": "wrongUsername",
+            "password": "wrongPassword"
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
