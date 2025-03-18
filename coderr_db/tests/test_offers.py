cb6c04c501008e54f468ec43863b98e65f4779af
Alexander Hardtke <5yl3kz@googlemail.com>
2025-03-18 17:08:31 +0100

    offers setup started

--- coderr_db/tests/test_offers.py
+++ coderr_db/tests/test_offers.py
@@ -9,14 +9,55 @@ from coderr_db.models import Offer
 class OfferTests(APITestCase):
 
     def setUp(self):
-        self.user = User.objects.create_user(
-            username='testuser', password='testpassword')
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.user_Offer = Offer.objects.create(
+                title = 'Grafikdesign-Paket',
+                image = None,
+                description = 'Ein umfassendes Grafikdesign-Paket für Unternehmen.',
+                details = [
+                    {
+                        "title": "Basic Design",
+                        "revisions": 2,
+                        "delivery_time_in_days": 5,
+                        "price": 100,
+                        "features": [
+                            "Logo Design",
+                            "Visitenkarte"
+                        ],
+                        "offer_type": "basic"
+                    },
+                    {
+                        "title": "Standard Design",
+                        "revisions": 5,
+                        "delivery_time_in_days": 7,
+                        "price": 200,
+                        "features": [
+                            "Logo Design",
+                            "Visitenkarte",
+                            "Briefpapier"
+                        ],
+                        "offer_type": "standard"
+                    },
+                    {
+                        "title": "Premium Design",
+                        "revisions": 10,
+                        "delivery_time_in_days": 10,
+                        "price": 500,
+                        "features": [
+                            "Logo Design",
+                            "Visitenkarte",
+                            "Briefpapier",
+                            "Flyer"
+                        ],
+                        "offer_type": "premium"
+                    }
+                ]
+        )
         self.client = APIClient
         self.token = Token.objects.create(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-        self.url = reverse('')
+        self.url = reverse('offers-list')
 
     def test_get_offer(self):
-        url = reverse('offers-list')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
--- coderr_db/tests/test_orders.py
+++ coderr_db/tests/test_orders.py
@@ -1,4 +1,36 @@
-# from rest_framework.test import APITestCase
+# from django.urls import reverse
+# from rest_framework.test import APITestCase, APIClient
+# from rest_framework import status
+# from rest_framework.authtoken.models import Token
+# from django.contrib.auth.models import User
+# from coderr_db.models import Order
 
-# class OrderTests(APITestCase):
-#     pass
+
+# class OrdersTests(APITestCase):
+
+#     def setUp(self):
+#         self.user = User.objects.create_user(
+#             username='testuser', password='testpassword')
+#         self.order = Order.objects.create(
+#             id=1,
+#             customer_user=self.user,
+#             business_user=User.objects.create_user(username='businessuser', password='businesspassword'),
+#             title="Logo Design",
+#             revisions=3,
+#             delivery_time_in_days=5,
+#             price=150,
+#             features=["Logo Design", "Visitenkarten"],
+#             offer_type="basic",
+#             status="in_progress",
+#             created_at=make_aware(datetime(2024, 9, 29, 10, 0, 0)),
+#             updated_at=make_aware(datetime(2024, 9, 30, 12, 0, 0))
+#         )
+#         self.client = APIClient
+#         self.token = Token.objects.create(user=self.user)
+#         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+#         self.url = reverse('orders-list')
+
+#     def test_get_offer(self):
+#         url = reverse('offers-list')
+#         response = self.client.get(url)
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
