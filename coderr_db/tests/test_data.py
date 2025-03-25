c818168bd959daa55905f3095e3ef03c4c7c6a70
Alexander Hardtke <5yl3kz@googlemail.com>
2025-03-25 10:32:28 +0100

    created customer users correctly in tests

--- coderr_db/tests/test_data.py
+++ coderr_db/tests/test_data.py
@@ -1,4 +1,4 @@
-from coderr_db.models import Offer, Order, UserProfil
+from coderr_db.models import Offer, Order, UserProfil, OfferDetail
 from django.contrib.auth.models import User
 
 
@@ -72,102 +72,113 @@ new_review_data = {
 }
 
 
-def create_test_offers():
-    return [
-        Offer.objects.create(
-            title='API-Paket',
-            image=None,
-            description='Ein umfassendes API-Paket für Unternehmen.',
-            details=[
-                {
-                    "title": "Basic API",
-                    "revisions": 2,
-                    "delivery_time_in_days": 6,
-                    "price": 150,
-                    "features": ["Login", "Registrierung"],
-                    "offer_type": "basic"
-                },
-                {
-                    "title": "Standard API",
-                    "revisions": 5,
-                    "delivery_time_in_days": 8,
-                    "price": 300,
-                    "features": ["Login", "Registrierung", "Bearbeitung einfacher Datenstrukturen"],
-                    "offer_type": "standard"
-                },
-                {
-                    "title": "Premium API",
-                    "revisions": 10,
-                    "delivery_time_in_days": 15,
-                    "price": 750,
-                    "features": ["Login", "Registrierung", "Bearbeitung einfacher und komplexer Datenstrukturen"],
-                    "offer_type": "premium"
-                }
-            ]
-        ),
-        Offer.objects.create(
-            title='Grafikdesign-Paket',
-            image=None,
-            description='Ein umfassendes Grafikdesign-Paket für Unternehmen.',
-            details=[
-                {
-                    "title": "Basic Design",
-                    "revisions": 2,
-                    "delivery_time_in_days": 5,
-                    "price": 100,
-                    "features": ["Logo Design", "Visitenkarte"],
-                    "offer_type": "basic"
-                },
-                {
-                    "title": "Standard Design",
-                    "revisions": 5,
-                    "delivery_time_in_days": 7,
-                    "price": 200,
-                    "features": ["Logo Design", "Visitenkarte", "Briefpapier"],
-                    "offer_type": "standard"
-                },
-                {
-                    "title": "Premium Design",
-                    "revisions": 10,
-                    "delivery_time_in_days": 10,
-                    "price": 500,
-                    "features": ["Logo Design", "Visitenkarte", "Briefpapier", "Flyer"],
-                    "offer_type": "premium"
-                }
-            ]
-        ),
-        Offer.objects.create(
-            title='App-Umwandlung',
-            image=None,
-            description='Ein umwandlung ihrer Website in eine Smartphone-App.',
-            details=[
-                {
-                    "title": "Android",
-                    "revisions": 2,
-                    "delivery_time_in_days": 3,
-                    "price": 100,
-                    "features": ["Funktional in Android-Store"],
-                    "offer_type": "basic"
-                },
-                {
-                    "title": "Apple-IOS",
-                    "revisions": 3,
-                    "delivery_time_in_days": 5,
-                    "price": 150,
-                    "features": ["Funktional in Apple-Store", "Geprüft durch Apple"],
-                    "offer_type": "standard"
-                },
-                {
-                    "title": "Android + Apple-IOS",
-                    "revisions": 6,
-                    "delivery_time_in_days": 7,
-                    "price": 225,
-                    "features": ["Funktional in Android-Store", "Funktional in Apple-Store", "Geprüft durch Apple"],
-                    "offer_type": "premium"
-                }
-            ]
-        )
-    ]
+def create_test_offers(user):
+    offer1 = Offer.objects.create(
+        title='API-Paket',
+        image=None,
+        description='Ein umfassendes API-Paket für Unternehmen.',
+        user=user
+    )
+    
+    offer2 = Offer.objects.create(
+        title='Grafikdesign-Paket',
+        image=None,
+        description='Ein umfassendes Grafikdesign-Paket für Unternehmen.',
+        user=user
+    )
+    
+    offer3 = Offer.objects.create(
+        title='App-Umwandlung',
+        image=None,
+        description='Ein umwandlung ihrer Website in eine Smartphone-App.',
+        user=user
+    )
+    
+    OfferDetail.objects.create(
+        offer=offer1,
+        title="Basic API",
+        revisions=2,
+        delivery_time_in_days=6,
+        price=150,
+        features=["Login", "Registrierung"],
+        offer_type="basic"
+    )
+    OfferDetail.objects.create(
+        offer=offer1,
+        title="Standard API",
+        revisions=5,
+        delivery_time_in_days=8,
+        price=300,
+        features=["Login", "Registrierung", "Bearbeitung einfacher Datenstrukturen"],
+        offer_type="standard"
+    )
+    OfferDetail.objects.create(
+        offer=offer1,
+        title="Premium API",
+        revisions=10,
+        delivery_time_in_days=15,
+        price=750,
+        features=["Login", "Registrierung", "Bearbeitung einfacher und komplexer Datenstrukturen"],
+        offer_type="premium"
+    )
+    
+    OfferDetail.objects.create(
+        offer=offer2,
+        title="Basic Design",
+        revisions=2,
+        delivery_time_in_days=5,
+        price=100,
+        features=["Logo Design", "Visitenkarte"],
+        offer_type="basic"
+    )
+    OfferDetail.objects.create(
+        offer=offer2,
+        title="Standard Design",
+        revisions=5,
+        delivery_time_in_days=7,
+        price=200,
+        features=["Logo Design", "Visitenkarte", "Briefpapier"],
+        offer_type="standard"
+    )
+    OfferDetail.objects.create(
+        offer=offer2,
+        title="Premium Design",
+        revisions=10,
+        delivery_time_in_days=10,
+        price=500,
+        features=["Logo Design", "Visitenkarte", "Briefpapier", "Flyer"],
+        offer_type="premium"
+    )
+    
+    OfferDetail.objects.create(
+        offer=offer3,
+        title="Android",
+        revisions=2,
+        delivery_time_in_days=3,
+        price=100,
+        features=["Funktional in Android-Store"],
+        offer_type="basic"
+    )
+    OfferDetail.objects.create(
+        offer=offer3,
+        title="Apple-IOS",
+        revisions=3,
+        delivery_time_in_days=5,
+        price=150,
+        features=["Funktional in Apple-Store", "Geprüft durch Apple"],
+        offer_type="standard"
+    )
+    OfferDetail.objects.create(
+        offer=offer3,
+        title="Android + Apple-IOS",
+        revisions=6,
+        delivery_time_in_days=7,
+        price=225,
+        features=["Funktional in Android-Store", "Funktional in Apple-Store", "Geprüft durch Apple"],
+        offer_type="premium"
+    )
+    
+    return [offer1, offer2, offer3]
 
 
 def create_test_orders():
--- coderr_db/tests/test_offers.py
+++ coderr_db/tests/test_offers.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from coderr_db.models import Offer
 from coderr_db.api.serializers import OfferSerializer
-from .test_data import create_business_user, create_test_offers, new_offer_data, invalid_offer_pk, patched_offer_data, invalid_offer_data, offer_detail
+from .test_data import create_business_user, create_customer_user, create_test_offers, new_offer_data, invalid_offer_pk, patched_offer_data, invalid_offer_data, offer_detail
 
 
 class OfferTests(APITestCase):
@@ -15,11 +15,11 @@ class OfferTests(APITestCase):
             username='businessuser', password='businesspw'
         )
         self.user_profile = create_business_user(self.user)
-        self.user_offers = create_test_offers()
-        self.client = APIClient
+        self.user_offers = create_test_offers(user=self.user_profile[0])
+        self.client = APIClient()
         self.token = Token.objects.create(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-        self.url = reverse('offers-list')
+        self.url = reverse('offer-list')
 
     def test_get_offer_list(self):
         response = self.client.get(self.url, {'min_price': 100})
@@ -56,14 +56,15 @@ class OfferTests(APITestCase):
         self.user = User.objects.create_user(
             username='customer', password='customerpassword'
         )
-        self.client = APIClient
+        self.user_profile = create_customer_user(self.user)
+        self.client = APIClient()
         self.token = Token.objects.create(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
         response = self.client.post(self.url, new_offer_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_get_offer_single(self):
-        url = reverse('offers-detail', kwargs={'pk': self.user_offers[0].pk})
+        url = reverse('offer-detail', kwargs={'pk': self.user_offers[0].pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -71,32 +72,33 @@ class OfferTests(APITestCase):
         unauthorized_token = 'unauthorized token'
         self.client.credentials(
             HTTP_AUTHORIZATION='Token ' + unauthorized_token)
-        url = reverse('offers-detail', kwargs={'pk': self.user_offers[0].pk})
+        url = reverse('offer-detail', kwargs={'pk': self.user_offers[0].pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_not_found_get_offer_single(self):
-        url = reverse('offers-detail', kwargs={'pk': invalid_offer_pk})
+        url = reverse('offer-detail', kwargs={'pk': invalid_offer_pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_patch_offer(self):
-        url = reverse('offers-detail', kwargs={'pk': self.user_offers[0].pk})
+        url = reverse('offer-detail', kwargs={'pk': self.user_offers[0].pk})
         response = self.client.patch(url, patched_offer_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["status"], "completed")
 
     def test_invalid_patch_offer(self):
-        url = reverse('offers-detail', kwargs={'pk': self.user_offers[0].pk})
+        url = reverse('offer-detail', kwargs={'pk': self.user_offers[0].pk})
         response = self.client.patch(url, invalid_offer_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_unauthorized_patch_offer(self):
-        url = reverse('offers-detail', kwargs={'pk': self.user_offers[0].pk})
+        url = reverse('offer-detail', kwargs={'pk': self.user_offers[0].pk})
         self.user = User.objects.create_user(
             username='customer', password='customerpassword'
         )
-        self.client = APIClient
+        self.user_profile = create_customer_user(self.user)
+        self.client = APIClient()
         self.token = Token.objects.create(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
         response = self.client.patch(url, patched_offer_data, format='json')
@@ -109,23 +111,24 @@ class OfferTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_not_found_patch_offer(self):
-        url = reverse('offers-detail', kwargs={'pk': invalid_offer_pk})
+        url = reverse('offer-detail', kwargs={'pk': invalid_offer_pk})
         response = self.client.patch(url, patched_offer_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_delete_offer(self):
-        url = reverse('offers-detail', kwargs={'pk': self.user_offers[0].pk})
+        url = reverse('offer-detail', kwargs={'pk': self.user_offers[0].pk})
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_unauthorized_delete_offer(self):
-        url = reverse('offers-detail', kwargs={'pk': self.user_offers[0].pk})
+        url = reverse('offer-detail', kwargs={'pk': self.user_offers[0].pk})
         self.user = User.objects.create_user(
             username='customer', password='customerpassword'
         )
-        self.client = APIClient
+        self.user_profile = create_customer_user(self.user)
+        self.client = APIClient()
         self.token = Token.objects.create(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
         response = self.client.delete(url)
@@ -138,7 +141,7 @@ class OfferTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_not_found_delete_offer(self):
-        url = reverse('offers-detail', kwargs={'pk': invalid_offer_pk})
+        url = reverse('offer-detail', kwargs={'pk': invalid_offer_pk})
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
