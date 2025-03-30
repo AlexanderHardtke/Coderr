87bb1fec334d4f576c98693a1aa363cb07002911
Alexander Hardtke <5yl3kz@googlemail.com>
2025-03-30 09:12:58 +0200

    completed debugging test orders

--- coderr_db/tests/test_orders.py
+++ coderr_db/tests/test_orders.py
@@ -25,150 +25,155 @@ class OrderTests(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
         self.url = reverse('orders-list')
 
-    # def test_get_order_list(self):
-    #     response = self.client.get(self.url)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     first_order = response.data[0]
-    #     self.assertEqual(first_order['customer_user'], self.user.pk)
+    def test_get_order_list(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        first_order = response.data[0]
+        self.assertEqual(first_order['customer_user'], self.user.pk)
 
-    # def test_unauthorized_get_order_list(self):
-    #     unauthorized_token = 'unauthorized token'
-    #     self.client.credentials(
-    #         HTTP_AUTHORIZATION='Token ' + unauthorized_token
-    #     )
-    #     response = self.client.get(self.url)
-    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_unauthorized_get_order_list(self):
+        unauthorized_token = 'unauthorized token'
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + unauthorized_token
+        )
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    # def test_post_order(self):
-    #     data = {"offer_detail_id": 1}
-    #     response = self.client.post(self.url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    def test_post_order(self):
+        data = {"offer_detail_id": 1}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    # def test_invalid_post_order(self):
-    #     data = {"offer_error_id": 1}
-    #     response = self.client.post(self.url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_invalid_post_order(self):
+        data = {"offer_error_id": 1}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # def test_unauthorized_post_order(self):
-    #     data = {"offer_detail_id": 1}
-    #     self.token = Token.objects.create(user=self.business_user)
-    #     self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-    #     response = self.client.post(self.url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_unauthorized_post_order(self):
+        data = {"offer_detail_id": 1}
+        self.token = Token.objects.create(user=self.business_user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    #     unauthorized_token = 'unauthorized token'
-    #     self.client.credentials(
-    #         HTTP_AUTHORIZATION='Token ' + unauthorized_token
-    #     )
-    #     response = self.client.post(self.url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        unauthorized_token = 'unauthorized token'
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + unauthorized_token
+        )
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    # def test_invalid_post_order(self):
-    #     data = {"offer_detail_id": invalid_order_pk}
-    #     response = self.client.post(self.url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    def test_invalid_post_order(self):
+        data = {"offer_detail_id": invalid_order_pk}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    # def test_patch_order(self):
-    #     self.token = Token.objects.create(user=self.business_user)
-    #     self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-    #     url = reverse('orders-detail', kwargs={'pk': self.user_orders[0].pk})
-    #     data = {"status": "completed"}
-    #     response = self.client.patch(url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(response.data["status"], "completed")
+    def test_patch_order(self):
+        self.token = Token.objects.create(user=self.business_user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        url = reverse('orders-detail', kwargs={'pk': self.user_orders[0].pk})
+        data = {"status": "completed"}
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["status"], "completed")
 
-    # def test_invalid_patch_order(self):
-    #     self.token = Token.objects.create(user=self.business_user)
-    #     self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-    #     url = reverse('orders-detail', kwargs={'pk': self.user_orders[0].pk})
-    #     invalid_data = {"status": "error"}
-    #     response = self.client.patch(url, invalid_data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    def test_invalid_patch_order(self):
+        self.token = Token.objects.create(user=self.business_user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        url = reverse('orders-detail', kwargs={'pk': self.user_orders[0].pk})
+        invalid_data = {"status": "error"}
+        response = self.client.patch(url, invalid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # def test_unauthorized_patch_order(self):
-    #     data = {"status": "completed"}
-    #     url = reverse('orders-detail', kwargs={'pk': self.user_orders[0].pk})
-    #     response = self.client.patch(url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_unauthorized_patch_order(self):
+        data = {"status": "completed"}
+        url = reverse('orders-detail', kwargs={'pk': self.user_orders[0].pk})
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    #     unauthorized_token = 'unauthorized token'
-    #     self.client.credentials(
-    #         HTTP_AUTHORIZATION='Token ' + unauthorized_token
-    #     )
-    #     response = self.client.patch(url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        unauthorized_token = 'unauthorized token'
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + unauthorized_token
+        )
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    # def test_not_found_patch_order(self):
-    #     data = {"status": "completed"}
-    #     url = reverse('orders-detail', kwargs={'pk': invalid_order_pk})
-    #     response = self.client.patch(url, data, format='json')
-    #     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    def test_not_found_patch_order(self):
+        data = {"status": "completed"}
+        url = reverse('orders-detail', kwargs={'pk': invalid_order_pk})
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    # def test_delete_order(self):
-    #     self.admin = User.objects.create_superuser(username='staff', password='staffpw', is_staff=True)
-    #     self.client.force_authenticate(user=self.admin)
-    #     url = reverse('orders-detail', kwargs={'pk': self.user_orders[0].pk})
-    #     response = self.client.delete(url)
-    #     self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    def test_delete_order(self):
+        self.admin = User.objects.create_superuser(username='staff', password='staffpw', is_staff=True)
+        self.client.force_authenticate(user=self.admin)
+        url = reverse('orders-detail', kwargs={'pk': self.user_orders[0].pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    # def test_unauthorized_delete_order(self):
-    #     url = reverse('orders-detail', kwargs={'pk': self.user_orders[0].pk})
-    #     response = self.client.delete(url)
-    #     self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_unauthorized_delete_order(self):
+        url = reverse('orders-detail', kwargs={'pk': self.user_orders[0].pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    #     unauthorized_token = 'unauthorized token'
-    #     self.client.credentials(
-    #         HTTP_AUTHORIZATION='Token ' + unauthorized_token
-    #     )
-    #     response = self.client.delete(url)
-    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        unauthorized_token = 'unauthorized token'
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + unauthorized_token
+        )
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    # def test_not_found_delete_order(self):
-    #     self.admin = User.objects.create_superuser(username='staff', password='staffpw', is_staff=True)
-    #     self.client.force_authenticate(user=self.admin)
-    #     url = reverse('orders-detail', kwargs={'pk': invalid_order_pk})
-    #     response = self.client.delete(url)
-    #     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    def test_not_found_delete_order(self):
+        self.admin = User.objects.create_superuser(username='staff', password='staffpw', is_staff=True)
+        self.client.force_authenticate(user=self.admin)
+        url = reverse('orders-detail', kwargs={'pk': invalid_order_pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    # def test_order_count_detail(self):
-    #     url = reverse('order-count-detail', kwargs={'pk': self.user.pk})
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertIsInstance(response.data['order_count'], int)
+    def test_order_count_detail(self):
+        url = reverse('order-count-detail', kwargs={'pk': self.business_user.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.data['order_count'], int)
 
-    # def test_unauthorized_order_count_detail(self):
-    #     unauthorized_token = 'unauthorized token'
-    #     self.client.credentials(
-    #         HTTP_AUTHORIZATION='Token ' + unauthorized_token
-    #     )
-    #     url = reverse('order-count-detail', kwargs={'pk': self.user.pk})
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_unauthorized_order_count_detail(self):
+        unauthorized_token = 'unauthorized token'
+        self.client.credentials(
+            HTTP_AUTHORIZATION='Token ' + unauthorized_token
+        )
+        url = reverse('order-count-detail', kwargs={'pk': self.user.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_not_found_order_count_detail(self):
         url = reverse('order-count-detail', kwargs={'pk': invalid_order_pk})
-        print(url)
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    # def test_order_count_completed_detail(self):
-    #     url = reverse(
-    #         'completed-order-count-detail', kwargs={'pk': self.user.pk}
-    #     )
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertIsInstance(response.data['completed_order_count'], int)
+    def test_order_count_completed_detail(self):
+        data = {"status": "completed"}
+        self.token = Token.objects.create(user=self.business_user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        url = reverse('orders-detail', kwargs={'pk': self.user_orders[0].pk})
+        response = self.client.patch(url, data, format='json')
+        completed_url = reverse(
+            'completed-order-count-detail', kwargs={'pk': self.business_user.pk}
+        )
+        
+        response = self.client.get(completed_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['order_count'], 1)
 
-    # def test_unauthorized_order_count_completed_detail(self):
-    #     url = reverse(
-    #         'completed-order-count-detail', kwargs={'pk': invalid_order_pk}
-    #     )
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    def test_unauthorized_order_count_completed_detail(self):
+        url = reverse(
+            'completed-order-count-detail', kwargs={'pk': invalid_order_pk}
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    # def test_not_found_order_count_completed_detail(self):
-    #     url = reverse(
-    #         'completed-order-count-detail', kwargs={'pk': invalid_order_pk}
-    #     )
-    #     response = self.client.get(url)
-    #     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    def test_not_found_order_count_completed_detail(self):
+        url = reverse(
+            'completed-order-count-detail', kwargs={'pk': invalid_order_pk}
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
